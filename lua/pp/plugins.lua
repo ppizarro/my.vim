ce6eb64329a8d0072094abde0011945ac63cce0a
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2022-06-26 20:08:23 -0300

    Improve bootstrap

--- lua/pp/plugins.lua
+++ lua/pp/plugins.lua
@@ -2,30 +2,20 @@ vim.cmd([[autocmd BufWritePost plugins.lua source <afile> | PackerCompile]])
 
 return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
-  use "lewis6991/impatient.nvim"
 
   -- visual
   use {
     "folke/tokyonight.nvim",
-    config = function()
-      require("pp.colors")
-    end,
   }
   use {"kyazdani42/nvim-web-devicons"}
   use {
     "hoob3rt/lualine.nvim",
-    config = function()
-      require("pp.lualine")
-    end,
   }
 
   -- navigation
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use {
     "nvim-telescope/telescope.nvim",
-    config = function()
-      require("pp.telescope")
-    end,
     requires = {
         "nvim-lua/popup.nvim",
         "nvim-lua/plenary.nvim",
@@ -34,9 +24,6 @@ return require("packer").startup(function(use)
   }
   use {
     "kyazdani42/nvim-tree.lua",
-    config = function()
-      require("pp.nvim-tree")
-    end,
     requires = {
         "kyazdani42/nvim-web-devicons",
     }
@@ -46,17 +33,11 @@ return require("packer").startup(function(use)
   use {"rafamadriz/friendly-snippets"}
   use{
     "L3MON4D3/LuaSnip",
-    config = function()
-      require("pp.luasnip")
-    end
   }
 
   -- completion
   use {
     "hrsh7th/nvim-cmp",
-    config = function()
-      require("pp.cmp")
-    end,
     requires = {
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-buffer",
@@ -72,9 +53,6 @@ return require("packer").startup(function(use)
   -- lsp
   use {
     "neovim/nvim-lspconfig",
-    config = function()
-      require("pp.lsp")
-    end,
     requires = {
       "williamboman/nvim-lsp-installer",
       --"glepnir/lspsaga.nvim",
@@ -86,9 +64,6 @@ return require("packer").startup(function(use)
   --[[
   use({
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("pp.treesitter")
-    end,
   })
   --use({ "nvim-treesitter/playground" })
   --]]
@@ -96,9 +71,6 @@ return require("packer").startup(function(use)
   -- git
   use {
     "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
     requires = {
         "nvim-lua/plenary.nvim",
     },
@@ -107,9 +79,6 @@ return require("packer").startup(function(use)
   -- comments
   use({
       "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup()
-      end,
   })
 
   -- lua dev
