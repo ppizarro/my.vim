6874a34d3e52089b9ac9022755db16fa9953cf8a
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2023-01-15 17:34:25 -0300

    Update treesitter config

--- lua/pp/plugins/treesitter.lua
+++ lua/pp/plugins/treesitter.lua
@@ -1,117 +1,116 @@
-local setup = function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "bash",
-            "diff",
-            "gitignore",
-            "go",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "regex",
-            "rust",
-            "yaml",
-            "json",
-        },
-        sync_install = false,
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = false },
-        context_commentstring = { enable = true, enable_autocmd = false },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = "<C-s>",
-                node_decremental = "<C-bs>",
-            },
-        },
-        query_linter = {
-            enable = true,
-            use_virtual_text = true,
-            lint_events = { "BufWrite", "CursorHold" },
-        },
-        playground = {
-            enable = true,
-            disable = {},
-            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-            persist_queries = true, -- Whether the query persists across vim sessions
-            keybindings = {
-                toggle_query_editor = "o",
-                toggle_hl_groups = "i",
-                toggle_injected_languages = "t",
-                toggle_anonymous_nodes = "a",
-                toggle_language_display = "I",
-                focus_language = "f",
-                unfocus_language = "F",
-                update = "R",
-                goto_node = "<cr>",
-                show_help = "?",
-            },
-        },
-        textobjects = {
-            select = {
-                enable = false,
-                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-            },
-            move = {
-                enable = false,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>a"] = "@parameter.inner",
-                },
-                swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
-                },
-            },
-            lsp_interop = {
-                enable = false,
-            },
-        },
-    })
-end
-
 return {
-    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-    --[[
   {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "BufReadPre",
-    config = true,
-  },
---]]
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = "BufReadPost",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "gitignore",
+        "go",
+        "gomod",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "rust",
+        "yaml",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<C-s>",
+          node_decremental = "<c-backspace>",
         },
-        config = setup,
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = true, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = false,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = false,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
+        },
+        lsp_interop = {
+          enable = false,
+        },
+      },
     },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "nvim-treesitter/playground",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "TSPlaygroundToggle",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    init = function()
+      require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+    end,
+  },
 }
