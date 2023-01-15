a12495dc64ba4f2b99ca23632e324e7d025212de
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2023-01-15 17:42:19 -0300

    Add config to go and lua files

---
+++ ftplugin/go.lua
@@ -0,0 +1,6 @@
+-- go configs
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.tabstop = 4
+vim.wo.colorcolumn = "80,120"
+vim.bo.expandtab = false
---
+++ ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- lua configs
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2
