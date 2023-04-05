9b4f4799a5a95ad6a4fff63adf5ad374a52a5748
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2023-04-05 12:41:40 -0300

    Update colorscheme settings

--- lua/pp/plugins/colorscheme.lua
+++ lua/pp/plugins/colorscheme.lua
@@ -7,18 +7,40 @@ return {
     priority = 1000,
     config = function()
       local nightfox = require("nightfox")
-      nightfox.setup()
+      local options = {
+        modules = {
+          cmp = true,
+          diagnostic = true,
+          gitsigns = true,
+          lsp_trouble = true,
+          native_lsp = true,
+          neotest = true,
+          neotree = true,
+          telescope = true,
+          treesitter = true,
+        },
+      }
+      local groups = {
+        all = {
+          Whitespace = { link = "Comment" },
+        },
+      }
+      nightfox.setup({ options = options, groups = groups })
       nightfox.load()
     end,
   },
 
+  --[[
   -- tokyonight
   {
     "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
     config = function()
       local tokyonight = require("tokyonight")
-      tokyonight.setup({ style = "moon" })
+      tokyonight.setup({ style = "night" })
       tokyonight.load()
     end,
   },
+--]]
 }
