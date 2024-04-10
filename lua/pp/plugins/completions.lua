5ba8f6ae87607adb0eaad427f47634e66d304d01
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2024-04-10 17:55:56 -0300

    Rename cmp to completions

--- lua/pp/plugins/completions.lua
+++ lua/pp/plugins/completions.lua
@@ -88,7 +88,13 @@ return {
           { name = "path" },
           { name = "buffer" },
         },
-        mapping = cmp.mapping.preset.insert(),
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
         formatting = {
           format = lspkind.cmp_format(),
         },
