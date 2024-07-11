e73d3a782c1448a08647ee647c31dc81fab1e999
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2024-07-11 09:55:21 -0300

    Update lua lsp settings

--- lua/pp/plugins/lsp/servers.lua
+++ lua/pp/plugins/lsp/servers.lua
@@ -6,13 +6,10 @@ local servers = {
         diagnostics = {
           -- Get the language server to recognize the `vim` global
           globals = { "vim" },
+          --disable = { "missing-fields" },
         },
-        workspace = {
-          checkThirdParty = false,
-          library = {
-            "${3rd}/luv/library",
-            unpack(vim.api.nvim_get_runtime_file("", true)),
-          },
+        completion = {
+          callSnippet = "Replace",
         },
       },
     },
