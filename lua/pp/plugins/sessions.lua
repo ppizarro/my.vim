805d2f598df47a4c1e107583a638ce4569b5b4bd
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2024-10-03 09:23:14 -0300

    Fix session config

--- lua/pp/plugins/sessions.lua
+++ lua/pp/plugins/sessions.lua
@@ -2,7 +2,7 @@ return {
   "rmagatti/auto-session",
   config = function()
     require("auto-session").setup({
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
       session_lens = {
         buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
         load_on_setup = true,
