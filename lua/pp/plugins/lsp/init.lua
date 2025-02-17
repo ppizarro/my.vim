2683208cb53598463658586aaf4b4c6c6e54d7f5
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2025-02-17 16:53:31 -0300

    feat(diagnostic): add diagnostic config (#1335)

--- lua/pp/plugins/lsp/init.lua
+++ lua/pp/plugins/lsp/init.lua
@@ -46,42 +46,34 @@ return {
         end,
       })
 
-      -- Change diagnostic symbols in the sign column (gutter)
-      if vim.g.have_nerd_font then
-        local signs = { ERROR = "", WARN = "", INFO = "", HINT = "" }
-        local diagnostic_signs = {}
-        for type, icon in pairs(signs) do
-          diagnostic_signs[vim.diagnostic.severity[type]] = icon
-        end
-        vim.diagnostic.config({
-          signs = { text = diagnostic_signs },
-          float = {
-            header = "",
-            focusable = false,
-            border = "rounded",
-            close_events = {
-              "BufLeave",
-              "CursorMoved",
-              "InsertEnter",
-              "FocusLost",
-            },
-            prefix = "",
-            suffix = "",
-            format = function(diagnostic)
-              if diagnostic.source == "rustc" and diagnostic.user_data.lsp.data ~= nil then
-                return diagnostic.user_data.lsp.data.rendered
-              else
-                return diagnostic.message
-              end
-            end,
+      -- Diagnostic Config
+      -- See :help vim.diagnostic.Opts
+      vim.diagnostic.config({
+        severity_sort = true,
+        float = { border = "rounded", source = "if_many" },
+        underline = { severity = vim.diagnostic.severity.ERROR },
+        signs = vim.g.have_nerd_font and {
+          text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
           },
-          --virtual_text = false,
-          -- update_in_insert = true,
-          virtual_text = { spacing = 4, prefix = "●" },
-          update_in_insert = false,
-          underline = true,
-        })
-      end
+        } or {},
+        virtual_text = {
+          source = "if_many",
+          spacing = 2,
+          format = function(diagnostic)
+            local diagnostic_message = {
+              [vim.diagnostic.severity.ERROR] = diagnostic.message,
+              [vim.diagnostic.severity.WARN] = diagnostic.message,
+              [vim.diagnostic.severity.INFO] = diagnostic.message,
+              [vim.diagnostic.severity.HINT] = diagnostic.message,
+            }
+            return diagnostic_message[diagnostic.severity]
+          end,
+        },
+      })
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
