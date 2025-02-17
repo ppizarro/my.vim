b3e34d54e65ae2ebba2258ced74a55fb36a7d07a
Paulo Pizarro <paulo.pizarro@neoway.com.br>
2025-02-17 16:43:08 -0300

    fix: arguments for the `vim.lsp.Client.supports_method` method (#1356)

--- lua/pp/plugins/lsp/highlights.lua
+++ lua/pp/plugins/lsp/highlights.lua
@@ -6,7 +6,7 @@ function M.on_attach(client, bufnr)
   --    See `:help CursorHold` for information about when this is executed
   --
   -- When you move your cursor, the highlights will be cleared (the second autocommand).
-  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
     local highlight_augroup = vim.api.nvim_create_augroup("pp-lsp-highlight", { clear = false })
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       buffer = bufnr,
--- lua/pp/plugins/lsp/keymaps.lua
+++ lua/pp/plugins/lsp/keymaps.lua
@@ -36,7 +36,7 @@ function M.on_attach(client, bufnr)
 
   -- The following code creates a keymap to toggle inlay hints in your
   -- code, if the language server you are using supports them
-  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
     map("<leader>th", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
     end, "[T]oggle Inlay [H]ints")
