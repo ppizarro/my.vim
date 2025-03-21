ppizarro/my.vim

commit b3e34d54e65ae2ebba2258ced74a55fb36a7d07a

fix: arguments for the `vim.lsp.Client.supports_method` method (#1356)

lua/pp/plugins/lsp/highlights.lua
@@ -6,7 +6,7 @@ function M.on_attach(client, bufnr)
   --    See `:help CursorHold` for information about when this is executed
   --
   -- When you move your cursor, the highlights will be cleared (the second autocommand).
-  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
     local highlight_augroup = vim.api.nvim_create_augroup("pp-lsp-highlight", { clear = false })
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       buffer = bufnr,

lua/pp/plugins/lsp/keymaps.lua
@@ -36,7 +36,7 @@ function M.on_attach(client, bufnr)
 
   -- The following code creates a keymap to toggle inlay hints in your
   -- code, if the language server you are using supports them
-  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
     map("<leader>th", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
     end, "[T]oggle Inlay [H]ints")

commit cbc20f29974a710184e80a45bf30756888446bff

fix(lsp): regression introduced in b3e34d54e65ae2ebba2258ced74a55fb36a7d07a

lua/pp/plugins/lsp/highlights.lua
@@ -6,7 +6,9 @@ function M.on_attach(client, bufnr)
   --    See `:help CursorHold` for information about when this is executed
   --
   -- When you move your cursor, the highlights will be cleared (the second autocommand).
-  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+
+  -- FIXME: for nvim-0.11 replace to client:supports_method(method, bufnr)
+  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, { bufnr = bufnr }) then
     local highlight_augroup = vim.api.nvim_create_augroup("pp-lsp-highlight", { clear = false })
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       buffer = bufnr,

lua/pp/plugins/lsp/init.lua
@@ -82,8 +82,6 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-      require("mason").setup()
-
       local servers = require("pp.plugins.lsp.servers")
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
@@ -97,11 +95,14 @@ return {
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
       require("mason-lspconfig").setup({
-        ensure_installed = {}, -- explicitly set to an empty table (we populates installs via mason-tool-installer)
+        ensure_installed = {}, -- explicitly set to an empty table (we populate installs via mason-tool-installer)
         automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
             require("lspconfig")[server_name].setup(server)
           end,

lua/pp/plugins/lsp/keymaps.lua
@@ -7,10 +7,17 @@ function M.on_attach(client, bufnr)
     vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
   end
 
+  -- Jump to the definition of the word under your cursor.
+  --  This is where a variable was first declared, or where a function is defined, etc.
+  --  To jump back, press <C-t>.
   map("gd", tb.lsp_definitions, "[G]oto [D]efinition")
-  map("gI", tb.lsp_implementations, "[G]oto [I]mplementation")
+
+  -- Find references for the word under your cursor.
   map("gr", tb.lsp_references, "[G]oto [R]eferences")
-  map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+  -- Jump to the implementation of the word under your cursor.
+  --  Useful when your language has ways of declaring types without an actual implementation.
+  map("gI", tb.lsp_implementations, "[G]oto [I]mplementation")
 
   -- Jump to the type of the word under your cursor.
   --  Useful when you're not sure what type a variable is and you want to see
@@ -25,18 +32,28 @@ function M.on_attach(client, bufnr)
   --  Similar to document symbols, except searches over your entire project.
   map("<leader>ws", tb.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-  -- rename
+  -- Rename the variable under your cursor.
+  --  Most Language Servers support renaming across files, etc.
   map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-  -- code action
+  -- Execute a code action, usually your cursor needs to be on top of an error
+  -- or a suggestion from your LSP for this to activate.
   map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
   -- signature
   map("<C-s>", vim.lsp.buf.signature_help, "Signature Documentation")
 
+  -- WARN: This is not Goto Definition, this is Goto Declaration.
+  --  For example, in C this would take you to the header.
+  map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
   -- The following code creates a keymap to toggle inlay hints in your
   -- code, if the language server you are using supports them
-  if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+  --
+  -- This may be unwanted, since they displace some of your code
+  --
+  -- FIXME: for nvim-0.11 replace to client:supports_method(method, bufnr)
+  if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, { bufnr = bufnr }) then
     map("<leader>th", function()
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }))
     end, "[T]oggle Inlay [H]ints")