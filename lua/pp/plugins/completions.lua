208a61365419f69541199ac78bea6e53e9ecad34
ppizarro <paulo.pizarro@gmail.com>
2025-04-15 07:11:14 -0300

    feat(blink): use rust as fuzzy matcher
    use rust as fuzzy matcher

--- lua/pp/plugins/completions.lua
+++ lua/pp/plugins/completions.lua
@@ -31,8 +31,6 @@ return { -- Autocompletion
       "fang2hou/blink-copilot",
       "folke/lazydev.nvim",
     },
-    --- @module 'blink.cmp'
-    --- @type blink.cmp.Config
     opts = {
       keymap = {
         -- 'default' (recommended) for mappings similar to built-in completions
@@ -91,13 +89,8 @@ return { -- Autocompletion
 
     -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
     -- which automatically downloads a prebuilt binary when enabled.
-    --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
     -- See :h blink-cmp-config-fuzzy for more information
-    -- fuzzy = { implementation = "prefer_rust_with_warning" }
-    fuzzy = { implementation = "lua" },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
 
     -- Shows a signature help window while you type arguments for a function
     signature = { enabled = true },
