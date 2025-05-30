8e62d6561a585f9e3d16f11972966fddb76399cc
ppizarro <paulo.pizarro@gmail.com>
2025-05-30 10:19:12 -0300

    remove duplicated which-key group

--- lua/pp/plugins/which-key.lua
+++ lua/pp/plugins/which-key.lua
@@ -47,7 +47,6 @@ return { -- Useful plugin to show you pending keybinds.
 
     -- Document existing key chains
     spec = {
-      { "<leader>s", group = "[S]earch" },
       { "<leader>t", group = "[T]oggle" },
       { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
     },
