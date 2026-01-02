758a77153f8670dfeaa19b7eabbe6b2124022b15
ppizarro <paulo.pizarro@gmail.com>
2026-01-02 07:53:29 -0300

    fix(gitsigns): update git signs for better visibility

--- lua/pp/plugins/gitsigns.lua
+++ lua/pp/plugins/gitsigns.lua
@@ -3,12 +3,12 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "契" },
-        topdelete = { text = "契" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
       },
       on_attach = function(bufnr)
         local gitsigns = require("gitsigns")
