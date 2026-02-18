73d0e4eaa6874d5549559565cad5a437b00af1dc
ppizarro <paulo.pizarro@gmail.com>
2026-02-18 10:04:31 -0300

    fix: update main module reference for nvim-treesitter (#1832)

--- lua/pp/plugins/treesitter.lua
+++ lua/pp/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     --dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
+    main = "nvim-treesitter.config", -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
