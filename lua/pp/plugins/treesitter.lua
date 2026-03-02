b6d31b8e3a68db265fc633887d29b36d1c00dc00
ppizarro <paulo.pizarro@gmail.com>
2026-03-02 10:01:07 -0300

    fix: Attach treesitter using language name instead of filetype
    Add treesitter indentation

--- lua/pp/plugins/treesitter.lua
+++ lua/pp/plugins/treesitter.lua
@@ -1,10 +1,14 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  {
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    branch = 'main',
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local filetypes = {
+      local parsers = {
         'bash',
         'gitignore',
         'go',
@@ -25,10 +29,28 @@ return {
         'vimdoc',
         'yaml',
       }
-      require('nvim-treesitter').install(filetypes)
+      require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = filetypes,
-        callback = function() vim.treesitter.start() end,
+        pattern = parsers,
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then return end
+
+          -- check if parser exists and load it
+          if not vim.treesitter.language.add(language) then return end
+          -- enables syntax highlighting and other treesitter features
+          vim.treesitter.start(buf, language)
+
+          -- enables treesitter based folds
+          -- for more info on folds see `:help folds`
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+
+          -- enables treesitter based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
