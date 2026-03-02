83d4c05350ce6fd46a418c1ffdd9d2646dcdf71f
ppizarro <paulo.pizarro@gmail.com>
2026-03-02 09:08:18 -0300

    fix: Re-add mason-lspconfig

--- lua/pp/plugins/lsp/init.lua
+++ lua/pp/plugins/lsp/init.lua
@@ -17,7 +17,9 @@ return {
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
-      { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+      -- Maps LSP server names between nvim-lspconfig and Mason package names.
+      'mason-org/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
