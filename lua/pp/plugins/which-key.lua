631d82926fed3e22737fab905b5bd94e0a6e0da0
ppizarro <paulo.pizarro@gmail.com>
2026-03-12 09:43:46 -0300

    chore: Clarify gitsigns keymap for which-key

--- lua/pp/plugins/which-key.lua
+++ lua/pp/plugins/which-key.lua
@@ -15,7 +15,7 @@ return { -- Useful plugin to show you pending keybinds.
     spec = {
       { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
       { '<leader>t', group = '[T]oggle' },
-      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
       { 'gr', group = 'LSP Actions', mode = { 'n' } },
     },
   },
