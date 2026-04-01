da2fce75cc2fe3b3ba16f8ba3ad15455008fa1e5
ppizarro <paulo.pizarro@gmail.com>
2026-04-01 16:31:23 -0300

    chore(lint): add a comment to the line plugin about installing markdownlint

--- lua/pp/plugins/lint.lua
+++ lua/pp/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        markdown = { 'markdownlint' }, -- Make sure to install `markdownlint` via mason / npm
         go = { 'golangcilint' },
         html = { 'tidy' },
         json = { 'jsonlint' },
