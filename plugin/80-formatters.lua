df5d909ccae9b0f30cc8afebc7dc20b254b01f6a
ppizarro <paulo.pizarro@gmail.com>
2026-04-18 10:05:25 -0300

    feat(formatter): Change format_on_save to a whitelist instead of a blacklist

--- plugin/80-formatters.lua
+++ plugin/80-formatters.lua
@@ -10,14 +10,16 @@ vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
 require('conform').setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
-    -- Disable "format_on_save lsp_fallback" for languages that don't
-    -- have a well standardized coding style. You can add additional
-    -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true }
-    if disable_filetypes[vim.bo[bufnr].filetype] then
-      return nil
-    else
+    -- You can specify filetypes to autoformat on save here:
+    local enabled_filetypes = {
+      lua = true,
+      -- rust = true,
+      -- python = true,
+    }
+    if enabled_filetypes[vim.bo[bufnr].filetype] then
       return { timeout_ms = 500 }
+    else
+      return nil
     end
   end,
   default_format_opts = {
